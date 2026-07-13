ade46c5d1f45004bfd3b59a8ebba584c8f07094d
loizoupanagiotis82-del <loizoupanagiotis82@gmail.com>
2026-07-13 22:54:10 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,5 +1,22 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import BigInteger, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class Warning(Base):
+    __tablename__ = "warnings"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    guild_id: Mapped[int] = mapped_column(BigInteger)
+    user_id: Mapped[int] = mapped_column(BigInteger)
+    moderator_id: Mapped[int] = mapped_column(BigInteger)
+
+    reason: Mapped[str] = mapped_column(String(500))
